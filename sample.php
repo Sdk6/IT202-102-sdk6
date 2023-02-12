75f529fa3755e4e4399ed3ba7979ed5200a2e27d
Sdk6 <102538462+Sdk6@users.noreply.github.com>
2023-02-12 03:04:14 -0500

    commit message for practice

---
+++ sample.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>
+            Sample local page
+        </title>
+        <script>
+            const sample="123"
+            sample=456
+            console.log(sample+45)
+        </script>
+    </head>
+    <body>
+        <h2>PHP sample </h2>
+        <h1>
+            <?php
+                print(phpversion())
+            ?>
+            <hr />
+            <a href="https://apple.com">apple</a>
+        </h1>
+    </body>
+</html>
