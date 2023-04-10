603c7192e82ed06b3b8daed776ec438dcaf6a931
Sdk6 <102538462+Sdk6@users.noreply.github.com>
2023-04-10 18:51:44 -0400

    added creat table template

---
+++ project/sql/create-table-template.sql
@@ -0,0 +1,7 @@
+-- Active: 1675127507101@@db.ethereallab.app@3306@sdk6
+CREATE TABLE User_Roles(
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key'
+    create_time DATETIME COMMENT 'Create Time',
+    user_id int NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES Users(id)
+) COMMENT '';
